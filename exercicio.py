4e25216bc36a0a98259d92b39d2a2c4fea1ac2f7
DIEGO OLIVEIRA DOS SANTOS <00001094792962sp@aluno.educacao.sp.gov.br>
2024-05-22 17:36:04 -0300

    fist commit

---
+++ exercicio.py
@@ -0,0 +1,5 @@
+quantidade_desejada = int(input("Digite a quantidade desejada: "))
+if quantidade_desejada >=10:
+    print("Produto disponivel em estoque. aproveite!")
+else:
+    print("Produto indisponivel no momento. Volte mais tarde!")
